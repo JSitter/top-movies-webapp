656dbf054639de4fcadd016ff912ebb7c38e8d16
Justin Sitter <justin@jaytria.com>
2018-05-28 13:17:40 -0700

    Created routes

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,13 @@
 //We must make our routes available to the outside
+// by using module.exports
 module.exports = function(app){
-    
-}
+    //Main page
+    app.get('/', function(request, response){
+        response.render('all-movies')
+    });
+
+    //Get movie info
+    app.get('/info/:movie', function(request, response){
+        response.json({info: "Hello World"})
+    })
+};
