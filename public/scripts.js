8d00395b809289d1aa1956fb14e1e703f1e37b6c
Justin Sitter <justin@jaytria.com>
2018-05-28 19:28:41 -0700

    added content to pages

--- public/scripts.js
+++ public/scripts.js
@@ -1,10 +1,27 @@
-// fetch("https://douglas-adams-backend.herokuapp.com").then((res)=>{
-//     // console.log(res.text())
-//     // console.log(res.status)
-//     res.text()
-// }).then(( data)=>{
-//     console.log(data)
-// })
-let api_address = "https://douglas-adams-backend.herokuapp.com"
-fetch(api_address).then((response)=>response.text())
-.then((json)=> console.log(json))
+let api_address = "https://itunes.apple.com/us/rss/topmovies/limit=25/json"
+
+function getMoviesFromiTunes(){
+    return new Promise((resolve, reject)=>{
+        fetch(api_address).then((response)=>response.text())
+        .then((json)=>{
+
+            movies = JSON.parse(json)
+            movies = movies.feed.entry[0]
+            resolve(movies)
+        }).catch((err)=>{reject(err.message)})
+    })
+
+};
+
+getMoviesFromiTunes().then((json)=>{
+    console.log(json)
+})
+
+function getMovie(id){
+    let url = '/info/'+id.toString()
+    console.log(url)
+    window.open(url, "_self")
+
+}
+
+document.getElement
--- routes/router.js
+++ routes/router.js
@@ -3,38 +3,80 @@
 module.exports = function(app){
     //Import modules
     const fetch = require( 'node-fetch');
-    let api_address = "https://itunes.apple.com/us/rss/topmovies/limit=25/json";
+    const api_address = "https://itunes.apple.com/us/rss/topmovies/limit=25/json";
 
+    let moviesRaw = {}
+    let movies = {}
     //Helper functions here
     function getMoviesFromiTunes(){
-        return new Promise(function(resolve, reject){
+        console.log("getting movies")
+        return new Promise((resolve, reject)=>{
+            fetch(api_address).then((response)=>response.text())
+            .then((json)=>{
+                moviesRaw = JSON.parse(json)
+                moviesRaw = moviesRaw.feed.entry
+                for( let index in moviesRaw){
+                    let movie = {}
+                    movie["id"] = moviesRaw[index]["id"]["attributes"]["im:id"]
+                    movie["thumb-img"] = moviesRaw[index]["im:image"][0].label
+                    movie["img-link"] = moviesRaw[index]["im:image"][2].label
+                    movie["title"] = moviesRaw[index]["im:name"]["label"]
+                    movie["release-date"] = moviesRaw[index]["im:releaseDate"].attributes.label
+                    movie["price"] = moviesRaw[index]["im:price"].label
+                    movie["trailer"] = moviesRaw[index]["link"][1].attributes.href
+                    movie["summary"] = moviesRaw[index]["summary"].label
+                    movie["itunes-link"] = moviesRaw[index]["id"].label
 
-            fetch(api_address).then(function(response){
+                    movies[movie["id"]] = movie 
+                }
 
-                console.log("itunes returnd: ")
-                console.log(response)
-                resolve(response);
-            }).catch(function(err){reject(err)});
+                resolve(movies)
+            }).catch((err)=>{reject(err.message)})
         })
 
+
+
     };
+
+    //Get movie info
+    app.get('/info/:movie_id', function(request, response){
+        movie_id = request.params.movie_id
+
+        console.log("looking up " + movie_id)
+        if(Object.keys(movies).length = 1){
+            getMoviesFromiTunes().then((movies)=>{
+                console.log("Got response from iTunes")
+                console.log(movies[movie_id].title)
+                let movie = movies[movie_id]
+                console.log(movie)
+                response.render('single-movie', {movie:movie})
+            })
+        }else{
+            console.log("Getting saved response")
+            response.render('single-movie', movies[movie_id])
+        }
+
+    });
 
     //Main page
     app.get('/', function(request, response){
 
-        fetch(api_address).then((response)=>response.text())
-        .then((json)=> console.log(json))
+        getMoviesFromiTunes().then((movies)=>{
+            //response.render('all-movies', {movies: json.feed.entry})
+            // response.render('all-movies')
+            
+            response.render('all-movies', {movies: movies})
+                // console.log(json)
+                
+                // for( let index in movies.feed.entry){
+                //     response.render('all-movies', {movies: movies.feed.entry})
+                //     console.log(index)
+                // }
+                
+            
+        })
 
-        response.render('all-movies')
+        //response.render('all-movies')
 
-    });
-
-    //Get movie info
-    app.get('/info/:param', function(request, response){
-
-        response.json({
-                info: "Hello World", 
-                movie: request.params.param
-            });
     });
 };
